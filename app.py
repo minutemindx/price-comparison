01cd2257d02c58217b4dde2254f91d12d49f559c
Mlt <mac_book@192.168.1.117>
2025-06-24 17:06:39 +0300

    Türkçe karakter sorunu çözüldü

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Fiyat karşılaştırma sistemi başarıyla çalışıyor!"
+return "Fiyat karsilastirma sistemi basariyla calisiyor!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
